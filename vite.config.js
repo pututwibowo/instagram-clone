d3c286b106f3a11c35e7e1ada28ec2f5b8934348
pututwibowo <putut.info@gmail.com>
2024-05-17 05:27:27 +0700

    menambahkan package hot reload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import livewire from '@defstudio/vite-livewire-plugin';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,7 +10,11 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: false,
+        }),
+
+        livewire({  // <-- add livewire plugin
+            refresh: ['resources/css/app.css'],  // <-- will refresh css (tailwind ) as well
         }),
     ],
 });
